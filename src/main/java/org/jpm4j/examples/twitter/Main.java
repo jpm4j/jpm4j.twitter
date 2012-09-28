ef9b3ec4510176debab9340d1f20978845f8975d
Peter Kriens <peter.kriens@aqute.biz>
2012-09-28 11:47:52 +0200

    Added maven repo

--- src/main/java/org/jpm4j/examples/twitter/Main.java
+++ src/main/java/org/jpm4j/examples/twitter/Main.java
@@ -5,12 +5,12 @@ import twitter4j.*;
 public class Main {
 
 	public static void main(String args[]) throws TwitterException {
+		System.out.println("JTwitter");
 		Twitter twitter = new TwitterFactory().getInstance();
-		Query query = new Query("osgi");
+		Query query = new Query(args.length == 0 ? "java" : args[0]);
 		QueryResult result = twitter.search(query);
 		for (Tweet tweet : result.getTweets()) {
 			System.out.printf("@%-20s %s\n", tweet.getFromUser(), tweet.getText());
 		}
 	}
-
 }
